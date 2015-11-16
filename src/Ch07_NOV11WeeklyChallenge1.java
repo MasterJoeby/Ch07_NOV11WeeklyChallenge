d21974e16256271ee476af19efe7285b20fda092
MasterJoeby <170316@hkis.edu.hk>
2015-11-17 07:59:38 +0800

    Nov11_WeeklyChallenge
    This is my attempt at the weekly challenge

---
+++ src/Ch07_NOV11WeeklyChallenge1.java
@@ -0,0 +1,16 @@
+
+public class Ch07_NOV11WeeklyChallenge1 {
+	public static void main(String[] args) {
+		int n = 5;
+		for(int row = 1; row <= n; row++) 
+		{
+			for(int count=1; count <= row; count++) 
+				System.out.print("*");
+				System.out.println();
+			
+		}
+}
+}
+		
+
+	
---
+++ src/Ch07_NOV11WeeklyChallenge2.java
@@ -0,0 +1,28 @@
+
+public class Ch07_NOV11WeeklyChallenge2 {
+	public static void main(String[] args) {
+		int n = 9;
+		for(int row = 1; row <= n; row+=2) 
+		{	
+			for(int count=1; count <= row; count++)
+			System.out.print("*");
+			System.out.println();
+
+}
+		System.out.println();
+		System.out.println();
+		System.out.println();
+
+		
+		
+		
+		
+		n = 9;
+		for(int row = 1; row <= n; row+=2) 
+		{
+			for(int count=1; count <= row; count++) 
+				System.out.print("*");
+				System.out.println();
+}
+}
+}
